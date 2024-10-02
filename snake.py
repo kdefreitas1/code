615f9edd88e57c10263f8d5e02caadd17291ef5a
lego <kamalani.william@gmail.com>
2024-10-02 09:47:14 -0400

    moves circle

--- snake.py
+++ snake.py
@@ -2,7 +2,11 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
+clock = pygame.time.Clock()
 running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
     for event in pygame.event.get():
@@ -10,6 +14,24 @@ while running:
             running = False
 
     screen.fill("purple")
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
     pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
