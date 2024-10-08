ac95dd99f73455463d05fbd3ae9a6434c54de3b9
lego <kamalani.william@gmail.com>
2024-10-08 13:42:11 -0400

    moves rect only once

--- snake.py
+++ snake.py
@@ -13,21 +13,18 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP or event.key == pygame.K_w:
+                snake_y -= 10
+            elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                snake_y += 10
+            elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                snake_x -=10
+            elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                snake_x +=10
     screen.fill("purple")
 
     pygame.draw.rect(screen, "black",(snake_x,snake_y,50,50))
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        snake_y -= 10
-    if keys[pygame.K_s]:
-        snake_y += 10 
-    if keys[pygame.K_a]:
-        snake_x -= 10 
-    if keys[pygame.K_d]:
-        snake_x += 10 
-
     pygame.display.flip()
 
     clock.tick(60)
