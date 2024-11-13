dbcc1223d7544c5a227a7e4e35cac475e7d18b53
lego <kamalani.william@gmail.com>
2024-11-13 13:23:36 -0500

    added basic collisions function and basic end game function

--- snake.py
+++ snake.py
@@ -10,11 +10,23 @@ clock = pygame.time.Clock()
 running = True
 dx = 0
 dy = 0
-snakeBody = [pygame.Rect(360,360,GRID_SIZE,GRID_SIZE)]
-apple = pygame.Rect(random.randint(0, int((screen.get_width()-GRID_SIZE)/GRID_SIZE))*GRID_SIZE,random.randint(0, int((screen.get_height()-GRID_SIZE)/GRID_SIZE))*GRID_SIZE,GRID_SIZE,GRID_SIZE)
+snake_body = [pygame.Rect(360,360,GRID_SIZE,GRID_SIZE)]
+apple = pygame.Rect(random.randint(0, (screen.get_width()-GRID_SIZE)//GRID_SIZE)*GRID_SIZE,random.randint(0, int((screen.get_height()-GRID_SIZE)/GRID_SIZE))*GRID_SIZE,GRID_SIZE,GRID_SIZE)
+
+def end_game():
+    global running
+    running = False
+
+def check_collisions():
+    if snake_body[0].contains(apple):
+        apple.update(0,0,0,0)
+        apple.update(random.randint(0, (screen.get_width()-GRID_SIZE)//GRID_SIZE)*GRID_SIZE,random.randint(0, int((screen.get_height()-GRID_SIZE)/GRID_SIZE))*GRID_SIZE,GRID_SIZE,GRID_SIZE)
+    elif snake_body[0].x >= screen.get_width() or snake_body[0].x <= -GRID_SIZE or snake_body[0].y >= screen.get_height() or snake_body[0].y <= -GRID_SIZE:
+        end_game()
 
 def move_snake():
-    snakeBody[0].move_ip(dx,dy)
+    snake_body[0].move_ip(dx,dy)
+    check_collisions()
 
 while running:
     for event in pygame.event.get():
@@ -38,7 +50,7 @@ while running:
     pygame.draw.rect(screen, "red", apple) # draws apple
 
     # draws snake
-    for segment in snakeBody:
+    for segment in snake_body:
         pygame.draw.rect(screen,"green", segment)
 
     # draws vertical lines for grid
@@ -51,5 +63,5 @@ while running:
 
     move_snake()
     pygame.display.flip()
-    clock.tick(10)
+    clock.tick(12)
 pygame.quit()
