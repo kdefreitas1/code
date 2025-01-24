23a1494cdbc69ff435278f42cd5249069f9f3f1e
lego <kamalani.william@gmail.com>
2025-01-24 10:43:29 -0500

    fixed loop

--- guess num example.py
+++ guess num example.py
@@ -6,8 +6,7 @@ guess = int(input("Guess a number between 1 and 10: "))
 while guess != random_num:
     if guess > random_num:
         print("The number is lower!")
-        continue
     elif guess < random_num:
         print("The number is higher!")
-        continue
+    guess = int(input("Guess a number between 1 and 10: "))
 print("You guessed correctly!")
